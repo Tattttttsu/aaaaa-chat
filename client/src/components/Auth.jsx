53c0a076434827d857fb259c511f3d9cebbec205
Tattttttsu <iliinyh@mail.ru>
2023-02-13 10:44:22 +0500

    fix auth

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -28,7 +28,7 @@ const Auth = () => {
 
         const { username, password, phoneNumber, avatarURL } = form;
 
-        const URL = 'https://chatik.onrender.com';
+        const URL = 'https://chatik.onrender.com/auth';
         // const URL = 'https://medical-pager.herokuapp.com/auth';
 
         const { data: { token, userId, hashedPassword, fullName } } = await axios.post(`${URL}/${isSignup ? 'signup' : 'login'}`, {
